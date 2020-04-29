694452bdfc29d2793904011cbe4965d6cbe10178
quotto <rhyme.and.rhyme@gmail.com>
2020-04-29 22:45:57 +0900

    APIAdapterImplのテストのネットワークI/Oをすべてモック化

--- app/src/test/java/net/mythrowaway/app/adapter/APIAdapterImplTest.kt
+++ app/src/test/java/net/mythrowaway/app/adapter/APIAdapterImplTest.kt
@@ -1,168 +1,357 @@
 package net.mythrowaway.app.adapter
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.fuel.core.requests.DefaultBody
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import net.mythrowaway.app.domain.TrashData
 import net.mythrowaway.app.domain.TrashSchedule
 import org.junit.Assert
 import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.net.URL
 
 class APIAdapterImplTest {
     private val instance = APIAdapterImpl()
+
     @Test
-    fun syncTest(){
-        // テスト用データ（us-west-2）
-        //
+    fun syncTest() {
+        // id:8051b7f9eb654364ae77f0e770e347d2
         // description:[{\"id\":\"1234567\",\"type\":\"burn\",\"trash_val\":\"\",\"schedules\":[{\"type\":\"weekday\",\"value\":\"0\"},{\"type\":\"biweek\",\"value\":\"1-1\"}]},{\"id\":\"8901234\",\"type\":\"other\",\"trash_val\":\"空き缶\",\"schedules\":[{\"type\":\"evweek\",\"value\":{\"weekday\":\"2\",\"start\":\"2020-03-08\"}}]}]
         // timestamp: 1584691542469
+
+        val responseContent = """
+            {"id": "8051b7f9eb654364ae77f0e770e347d2","description": "[{\"id\":\"1234567\",\"type\":\"burn\",\"trash_val\":\"\",\"schedules\":[{\"type\":\"weekday\",\"value\":\"0\"},{\"type\":\"biweek\",\"value\":\"1-1\"}]},{\"id\":\"8901234\",\"type\":\"other\",\"trash_val\":\"空き缶\",\"schedules\":[{\"type\":\"evweek\",\"value\":{\"weekday\":\"2\",\"start\":\"2020-03-08\"}}]}]","timestamp": 1584691542469}
+        """
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 200,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
         val result = instance.sync("8051b7f9eb654364ae77f0e770e347d2")
-        Assert.assertEquals(2,result?.first?.size)
+        Assert.assertEquals(2, result?.first?.size)
         Assert.assertEquals("burn", result?.first?.get(0)?.type)
         Assert.assertEquals("biweek", result?.first?.get(0)?.schedules?.get(1)?.type)
         Assert.assertEquals("1-1", result?.first?.get(0)?.schedules?.get(1)?.value)
         Assert.assertEquals("other", result?.first?.get(1)?.type)
         Assert.assertEquals("空き缶", result?.first?.get(1)?.trash_val)
         Assert.assertEquals("evweek", result?.first?.get(1)?.schedules?.get(0)?.type)
-        Assert.assertEquals("2", (result?.first?.get(1)?.schedules?.get(0)?.value as HashMap<String,String>)["weekday"])
-        Assert.assertEquals("2020-03-08", (result?.first?.get(1)?.schedules?.get(0)?.value as HashMap<String,String>)["start"])
-        Assert.assertEquals(1584691542469,result?.second)
+        Assert.assertEquals(
+            "2",
+            (result?.first?.get(1)?.schedules?.get(0)?.value as HashMap<String, String>)["weekday"]
+        )
+        Assert.assertEquals(
+            "2020-03-08",
+            (result?.first?.get(1)?.schedules?.get(0)?.value as HashMap<String, String>)["start"]
+        )
+        Assert.assertEquals(1584691542469, result?.second)
     }
 
     @Test
-    fun syncTest_NotExistsID(){
+    fun syncTest_failed() {
+        val calculateLength: BodyLength = {"".length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream("".toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 500,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+
+        FuelManager.instance.client = mockClient
+
         val result = instance.sync("dummy")
-        Assert.assertEquals(null,result)
+        Assert.assertNull(result)
     }
 
     @Test
-    fun update() {
+    fun update_success() {
         val trash1 = TrashData().apply {
             id = "12345"
             type = "burn"
             schedules = arrayListOf(
-                TrashSchedule().apply{
-                type = "biweek"
-                value = "0-3"
-            }, TrashSchedule().apply{
-                type = "biweek"
-                value = "6-1"
-            })
+                TrashSchedule().apply {
+                    type = "biweek"
+                    value = "0-3"
+                }, TrashSchedule().apply {
+                    type = "biweek"
+                    value = "6-1"
+                })
         }
         val trash2 = TrashData().apply {
             id = "56789"
             type = "other"
             trash_val = "家電"
             schedules = arrayListOf(
-                TrashSchedule().apply{
-                type = "biweek"
-                value = "0-3"
-            })
+                TrashSchedule().apply {
+                    type = "biweek"
+                    value = "0-3"
+                })
         }
-        val result = instance.update("901d9db9-9723-4845-8929-b88814f82e49", arrayListOf(trash1,trash2))
-        println(result)
-        assert(result != null)
+
+        val responseContent = "{\"timestamp\": 123456789012345}"
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 200,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
+        val result =
+            instance.update("901d9db9-9723-4845-8929-b88814f82e49", arrayListOf(trash1, trash2))
+        Assert.assertEquals(123456789012345, result)
     }
 
     @Test
-    fun update_NotExistId() {
+    fun update_failed() {
         val trash1 = TrashData().apply {
+            id = "12345"
             type = "burn"
             schedules = arrayListOf(
-                TrashSchedule().apply{
-                type = "biweek"
-                value = "0-3"
-            },  TrashSchedule().apply{
-                type = "biweek"
-                value = "6-1"
-            })
+                TrashSchedule().apply {
+                    type = "biweek"
+                    value = "0-3"
+                }, TrashSchedule().apply {
+                    type = "biweek"
+                    value = "6-1"
+                })
         }
         val trash2 = TrashData().apply {
+            id = "56789"
             type = "other"
             trash_val = "家電"
             schedules = arrayListOf(
-                TrashSchedule().apply{
-                type = "biweek"
-                value = "0-3"
-            })
+                TrashSchedule().apply {
+                    type = "biweek"
+                    value = "0-3"
+                })
         }
-        val result = instance.update("dummy", arrayListOf(trash1,trash2))
-        Assert.assertEquals(null,result)
-    }
 
-    @Test
-    fun update_InvalidData() {
-        val result = instance.update("901d9db9-9723-4845-8929-b88814f82e49", arrayListOf())
-        Assert.assertEquals(null,result)
+        val responseContent = "{\"timestamp\": 123456789012345}"
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 500,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
+        val result =
+            instance.update("901d9db9-9723-4845-8929-b88814f82e49", arrayListOf(trash1, trash2))
+        Assert.assertNull(result)
     }
 
     @Test
     fun register() {
+        val responseContent = """
+            {"id": "8051b7f9eb654364ae77f0e770e347d2","timestamp": 1584691542469}
+        """
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 200,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
         val trash1 = TrashData().apply {
             id = "123456"
             type = "burn"
             schedules = arrayListOf(
-                TrashSchedule().apply{
-                type = "biweek"
-                value = "0-3"
-            },  TrashSchedule().apply{
-                type = "biweek"
-                value = "6-1"
-            })
+                TrashSchedule().apply {
+                    type = "biweek"
+                    value = "0-3"
+                }, TrashSchedule().apply {
+                    type = "biweek"
+                    value = "6-1"
+                })
         }
         val trash2 = TrashData().apply {
             id = "5678"
             type = "other"
             trash_val = "家電"
             schedules = arrayListOf(
-                TrashSchedule().apply{
-                type = "biweek"
-                value = "0-3"
-            })
+                TrashSchedule().apply {
+                    type = "biweek"
+                    value = "0-3"
+                })
         }
-        val result = instance.register(arrayListOf(trash1,trash2))
-        Assert.assertEquals(36,result?.first?.length)
-        assert(result?.second != null)
+        val result = instance.register(arrayListOf(trash1, trash2))
+        Assert.assertEquals("8051b7f9eb654364ae77f0e770e347d2", result?.first)
+        Assert.assertEquals(1584691542469, result?.second)
+
     }
 
     @Test
-    fun register_InvalidData() {
+    fun register_failed() {
+        val calculateLength: BodyLength = {"".length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream("".toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 500,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
         val result = instance.register(arrayListOf())
-        assert(result == null)
+        Assert.assertNull(result)
     }
 
     @Test
     fun publishActivationCode() {
+        val responseContent = "{\"code\": \"234567\"}"
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 200,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
         val result = instance.publishActivationCode("901d9db9-9723-4845-8929-b88814f82e49")
         println(result)
-        Assert.assertEquals(5,result?.length)
+        Assert.assertEquals("234567", result)
     }
 
     @Test
-    fun publishActivationCode_NotExistId(){
-        val result = instance.publishActivationCode("dummy")
-        assert(result == null)
+    fun publishActivationCode_failed() {
+        val responseContent = "{\"code\": \"234567\"}"
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 500,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
+        val result = instance.publishActivationCode("901d9db9-9723-4845-8929-b88814f82e49")
+        println(result)
+        Assert.assertNull(result)
     }
 
     @Test
     fun activate() {
-        // テスト用データ（us-west-2）
+        // テスト用データ
         // id:8051b7f9eb654364ae77f0e770e347d2
         // description:[{\"id\":\"1234567\",\"type\":\"burn\",\"trash_val\":\"\",\"schedules\":[{\"type\":\"weekday\",\"value\":\"0\"},{\"type\":\"biweek\",\"value\":\"1-1\"}]},{\"id\":\"8901234\",\"type\":\"other\",\"trash_val\":\"空き缶\",\"schedules\":[{\"type\":\"evweek\",\"value\":{\"weekday\":\"2\",\"start\":\"2020-03-08\"}}]}]
         // timestamp: 1584691542469
+
+        val responseContent = """
+            {"id": "8051b7f9eb654364ae77f0e770e347d2","description": "[{\"id\":\"1234567\",\"type\":\"burn\",\"trash_val\":\"\",\"schedules\":[{\"type\":\"weekday\",\"value\":\"0\"},{\"type\":\"biweek\",\"value\":\"1-1\"}]},{\"id\":\"8901234\",\"type\":\"other\",\"trash_val\":\"空き缶\",\"schedules\":[{\"type\":\"evweek\",\"value\":{\"weekday\":\"2\",\"start\":\"2020-03-08\"}}]}]","timestamp": 1584691542469}
+        """
+        val calculateLength: BodyLength = {responseContent.length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream(responseContent.toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 200,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
+
         val result = instance.activate("99999")
-        Assert.assertEquals("8051b7f9eb654364ae77f0e770e347d2",result?.id)
+        Assert.assertEquals("8051b7f9eb654364ae77f0e770e347d2", result?.id)
         Assert.assertEquals("burn", result?.scheduleList?.get(0)?.type)
         Assert.assertEquals("biweek", result?.scheduleList?.get(0)?.schedules?.get(1)?.type)
         Assert.assertEquals("1-1", result?.scheduleList?.get(0)?.schedules?.get(1)?.value)
         Assert.assertEquals("other", result?.scheduleList?.get(1)?.type)
         Assert.assertEquals("空き缶", result?.scheduleList?.get(1)?.trash_val)
         Assert.assertEquals("evweek", result?.scheduleList?.get(1)?.schedules?.get(0)?.type)
-        Assert.assertEquals("2", (result?.scheduleList?.get(1)?.schedules?.get(0)?.value as HashMap<String,String>)["weekday"])
-        Assert.assertEquals("2020-03-08", (result?.scheduleList?.get(1)?.schedules?.get(0)?.value as HashMap<String,String>)["start"])
-        Assert.assertEquals(1584691542469,result?.timestamp)
+        Assert.assertEquals(
+            "2",
+            (result?.scheduleList?.get(1)?.schedules?.get(0)?.value as HashMap<String, String>)["weekday"]
+        )
+        Assert.assertEquals(
+            "2020-03-08",
+            (result?.scheduleList?.get(1)?.schedules?.get(0)?.value as HashMap<String, String>)["start"]
+        )
+        Assert.assertEquals(1584691542469, result?.timestamp)
     }
 
     @Test
-    fun activate_NotExistCode() {
+    fun activate_failed() {
+        val calculateLength: BodyLength = {"".length.toLong()}
+        val openStream: BodySource = { ByteArrayInputStream("".toByteArray())}
+        val body = DefaultBody.from(
+            calculateLength = calculateLength,
+            openStream = openStream
+        )
+        val mockClient = mock<Client> {
+            onGeneric { executeRequest(any()) } doReturn Response(
+                statusCode = 500,
+                body = body,
+                url = URL("https://test.com")
+            )
+        }
+        FuelManager.instance.client = mockClient
         val result = instance.activate("dummy")
-        assert(result == null)
+        Assert.assertNull(result)
     }
 }
