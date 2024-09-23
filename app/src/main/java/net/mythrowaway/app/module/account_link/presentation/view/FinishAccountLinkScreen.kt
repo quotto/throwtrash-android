c587d7269f63060c2df998f5a0ff8dc0c1ea1918
quotto <5211139+quotto@users.noreply.github.com>
2024-09-23 21:39:23 +0900

    開発用AppLinkの追加

--- app/src/main/java/net/mythrowaway/app/module/account_link/presentation/view/FinishAccountLinkScreen.kt
+++ app/src/main/java/net/mythrowaway/app/module/account_link/presentation/view/FinishAccountLinkScreen.kt
@@ -3,13 +3,16 @@ package net.mythrowaway.app.module.account_link.presentation.view
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import net.mythrowaway.app.R
@@ -33,9 +36,13 @@ fun FinishAccountLinkScreen(
   if(uiState.finishAccountLink) {
     Log.d("EnableSkillScreen", "url: ${uiState.enableSkillUrl}")
     // WebViewを表示する
-    AndroidView(factory = {
-      WebView(it)
-    },
+    AndroidView(
+      modifier = Modifier.fillMaxSize(1f),
+      factory = {
+        val webView = WebView(it)
+        webView.setBackgroundColor(android.graphics.Color.WHITE)
+        webView
+      },
       update = { webView ->
         webView.apply {
           webViewClient = WebViewClient()
@@ -44,6 +51,8 @@ fun FinishAccountLinkScreen(
       })
   } else {
     Box(
+      modifier = Modifier.fillMaxSize(1f)
+        .background(color = androidx.compose.ui.graphics.Color.White),
       contentAlignment = Alignment.Center,
     ) {
       Text("Loading...")
